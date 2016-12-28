e51ab270faacac11d3ccc21a8672d39f45037897
Jarven <769558579@qq.com>
2016-12-28 19:31:47 +0800

    1.订单列表新增分页

--- src/app/page/order/order.component.ts
+++ src/app/page/order/order.component.ts
@@ -1,8 +1,8 @@
 /**
  * Created by yhm on 16/12/28.
  */
-import { Component, OnInit } from "@angular/core";
-import { OrderService } from "../../service/order.service";
+import {Component, OnInit} from "@angular/core";
+import {OrderService} from "../../service/order.service";
 
 @Component({
   moduleId: "OrderManage",
@@ -11,7 +11,7 @@ import { OrderService } from "../../service/order.service";
 })
 export class OrderComponent implements OnInit {
   //订单查询条件;
-  public orderReq:any = {
+  public orderReq: any = {
     keyType: "all",
     orderType: "0",
     orderStatus: "all",
@@ -20,15 +20,20 @@ export class OrderComponent implements OnInit {
     endTime: ""
   };
   //搜索提示
-  placeholder:string='这是搜索提示信息';
-  public orderList:any = [];
-  constructor(public orderService: OrderService){}
+  placeholder: string = '这是搜索提示信息';
+  //分页对象
+  pageOpts: any = {total: 100, limit: 3, perPage: 10, page: 1};
+  public orderList: any = [];
 
-  ngOnInit():void {
+  constructor(public orderService: OrderService) {
+  }
+
+  ngOnInit(): void {
     this.getOrderList(null);
   }
-  getOrderList(page:any): void{
-    if(!page){
+
+  getOrderList(page: any): void {
+    if (!page) {
       page = {
         page: 1,
         itemsPerPage: 10
@@ -37,11 +42,16 @@ export class OrderComponent implements OnInit {
 
     this.orderService.getOrderList(this.orderReq, page).subscribe(res => {
       this.orderList = res.json().rows;
-      this.orderList.forEach((r, index) =>{
-        console.log(r + "|"+index)
+      this.orderList.forEach((r, index) => {
+        console.log(r + "|" + index)
         r.rank = index + 1;
       });
       console.log(this.orderList)
     });
+  }
+
+  //分页事件
+  pageChange(event){
+
   }
 }
