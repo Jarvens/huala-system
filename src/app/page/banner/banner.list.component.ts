0fe37f90689f75dd112814446bbbe408fb0ada98
Jarven <769558579@qq.com>
2016-12-29 22:22:42 +0800

    1.修改table统一样式

--- src/app/page/banner/banner.list.component.ts
+++ src/app/page/banner/banner.list.component.ts
@@ -1,64 +1,73 @@
 import {Component, OnInit, ChangeDetectorRef} from '@angular/core';
-import {INglDatatableSort, INglDatatableRowClick} from 'ng-lightning/ng-lightning';
+import {INglDatatableSort, INglDatatableRowClick,} from 'ng-lightning/ng-lightning';
 import {BannerService} from '../../service/banner.service';
 import {Observable} from 'rxjs/Observable';
 @Component({
-    selector: 'banner-list-component',
-    templateUrl: 'banner.list.component.html'
+  selector: 'banner-list-component',
+  templateUrl: 'banner.list.component.html'
 })
 
 export class BannerListComponent implements OnInit {
-    //banner列表对象
-    bannerList: any = {};
-    //分页对象
-    pageOpts: any = {total: 0, limit: 3, perPage: 10}
-    //图片地址前缀
-    commonImgUrl = process.env.ImgUrl;
-    //搜索关键词
-    searchKey: string = '';
-    //是否展示提示信息
-    showAlert: boolean = false;
-    value: string;
-    placeholder: string = '搜索    名称';
+  //banner列表对象
+  bannerList: any = {};
+  //分页对象
+  pageOpts: any = {total: 0, limit: 3, perPage: 10}
+  //图片地址前缀
+  commonImgUrl = process.env.ImgUrl;
+  //搜索关键词
+  searchKey: string = '';
+  //是否展示提示信息
+  showAlert: boolean = false;
+  value: string;
+  placeholder: string = '搜索    名称';
+  //删除标记默认为false
+  delFlag: boolean = false;
 
-    constructor(private bannerService: BannerService, private cdr: ChangeDetectorRef) {
-    }
+  constructor(private bannerService: BannerService, private cdr: ChangeDetectorRef) {
+  }
 
-    ngOnInit(): void {
-        this.getBannerList(null, this.searchKey);
-    }
+  ngOnInit(): void {
+    this.getBannerList(null, this.searchKey);
+  }
 
-    //分页事件
-    pageChange(event) {
-        this.pageOpts.page = event;
-        this.getBannerList(this.pageOpts, this.searchKey);
-    }
+  //分页事件
+  pageChange(event) {
+    this.pageOpts.page = event;
+    this.getBannerList(this.pageOpts, this.searchKey);
+  }
 
-    //获取banner列表
-    getBannerList(page: any, searchKey: string) {
-        this.bannerService.getBannerList(page, searchKey).subscribe(res=> {
-            this.bannerList = res.json();
-            this.cdr.detectChanges();
-        });
-    }
+  //获取banner列表
+  getBannerList(page: any, searchKey: string) {
+    this.bannerService.getBannerList(page, searchKey).subscribe(res=> {
+      this.bannerList = res.json();
+      this.cdr.detectChanges();
+    });
+  }
 
-    //打开Toasth
-    show() {
-        this.showAlert = true;
-    }
+  //打开Toasth
+  show() {
+    this.showAlert = true;
+  }
 
-    //Table点击事件
-    onRowClick($event: INglDatatableRowClick) {
-    }
+  //Table点击事件
+  onRowClick($event: INglDatatableRowClick) {
+  }
 
-    //关闭Toast
-    onClose(reason: string) {
-        this.showAlert = false;
-    }
+  //关闭Toast
+  onClose(reason: string) {
+    this.showAlert = false;
+  }
 
-    //搜索方法
-    searchByCondition(event) {
-        this.searchKey = event;
-        this.getBannerList(this.pageOpts, this.searchKey);
-    }
+  //搜索方法
+  searchByCondition(event) {
+    this.searchKey = event;
+    this.getBannerList(this.pageOpts, this.searchKey);
+  }
+
+  //删除提示
+  delConfirm(event) {
+    this.delFlag = !this.delFlag;
+    console.log(this.delFlag);
+  }
+
 }
