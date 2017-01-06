df1a6d9416dbd3ece09963d8d89161dd79c2b047
Jarven <769558579@qq.com>
2017-01-06 13:42:13 +0800

    1.quillEditor

--- src/app/page/component/quilleditor/quill.editor.component.ts
+++ src/app/page/component/quilleditor/quill.editor.component.ts
@@ -1,19 +1,23 @@
 import {Component} from '@angular/core';
 @Component({
-  selector:'quill-editor-component',
-  templateUrl:'./quill.editor.component.html'
+  selector: 'quill-editor-component',
+  templateUrl: './quill.editor.component.html'
 })
 
-export class QuillEditorComponent{
-  editorContent:string =``;
-  editorConfig:any ={placeholder:"输入公告内容,支持html"};
+export class QuillEditorComponent {
+  editorContent: string = ``;
+  editorConfig: any = {placeholder: "输入公告内容,支持html"};
 
-  constructor(){}
-  onEditorCreated(quill){
-    console.log('this is quill object',quill);
+  constructor() {
   }
 
-  onContentChanged({ quill, html, text }) {
+  //初始化
+  onEditorCreated(quill) {
+
+  }
+
+  //内容改变
+  onContentChanged({quill, html, text}) {
     console.log(quill, html, text);
   }
 
