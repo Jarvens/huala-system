3254752c4d776cbe27cdebadd0438e3bc9d33deb
Jarven <769558579@qq.com>
2017-01-13 20:23:01 +0800

    1.布局修改

--- src/app/page/sellersubsidy/seller.subsidy.component.ts
+++ src/app/page/sellersubsidy/seller.subsidy.component.ts
@@ -1,10 +1,28 @@
-import {Component,OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 @Component({
-  selector:'seller-subsidy-component',
-  templateUrl:'./seller.subsidy.component.html'
+  selector: 'seller-subsidy-component',
+  templateUrl: './seller.subsidy.component.html'
 })
-export class SellerSubsidyComponent implements OnInit{
+export class SellerSubsidyComponent implements OnInit {
   ngOnInit(): void {
   }
-  constructor(){}
+
+  constructor() {
+  }
+
+  //tips  打开|关闭
+  open: boolean = false;
+  //奖励数据对象
+  rewardList: any = {};
+  //分页对象
+  pageOpts: any = {page: 1, total: 0, limit: 3, perPage: 10}
+
+  //初始化奖励数据
+  initData() {
+  }
+
+  //分页事件
+  pageChange(event) {
+
+  }
 }
