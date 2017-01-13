62f0983322ebdf893d87bfaadbfadf0bb817b6da
Jarven <769558579@qq.com>
2017-01-13 14:32:11 +0800

    1.日报

--- src/app/page/everydatereport/every.date.report.component.ts
+++ src/app/page/everydatereport/every.date.report.component.ts
@@ -2,20 +2,49 @@ import {Component} from '@angular/core';
 import {EveryDateReportService} from '../../service/every.date.report.service';
 @Component({
   selector: 'every-date-report-component',
-  templateUrl: './every.date.report.component.html'
+  templateUrl: './every.date.report.component.html',
+  styleUrls: ['./every.date.report.component.css']
 })
 
 export class EveryDateReportComponent {
 
-  constructor(private everyDateReportService:EveryDateReportService){}
-  //数据集合
-  reportDataList: any = {}
-  //分页对象
-  pageOpts: any = {page: 1, total: 0, limit: 3, perPage: 10}
+  constructor(private everyDateReportService: EveryDateReportService) {
+  }
 
-  //分页事件
-  pageChange(event) {
+  //每日报表数据对象
+  everyDateReport: any = {};
+  //Tips打开|关闭
+  open: boolean = false;
+  //日期对象
+  queryDate: string = '';
+  //Toast提示
+  toastMessage: string = '请选择日期';
+  //类型
+  toastType: string = 'warning';
+  //显示|关闭toast
+  showAlert: boolean = false;
 
+  //触发查询操作
+  receiveDate(event: any) {
+    this.queryDate = event;
+    this.everyDateReportService.getEveryDateReport(event).subscribe(res=> {
+      this.everyDateReport = res.json().body;
+    });
+  }
+
+  exportDataReport() {
+    if (!this.queryDate) {
+      this.showAlert = !this.showAlert;
+      return;
+    }
+    let city = localStorage.getItem("hualaCity");
+    this.everyDateReportService.exportDataReport(city,this.queryDate);
+
+  }
+
+  //toast通知
+  notifyParamFunction(event) {
+    this.showAlert = !this.showAlert;
   }
 
 }
--- src/app/service/every.date.report.service.ts
+++ src/app/service/every.date.report.service.ts
@@ -1,8 +1,19 @@
 import {Injectable} from '@angular/core';
 import {MyHttp} from '../core/http';
+const ApiUrl = process.env.ApiUrl;
 @Injectable()
 export class EveryDateReportService {
-  constructor(private Http:MyHttp){}
+  constructor(private http: MyHttp) {
+  }
 
+  //查询每日报表统计
+  getEveryDateReport(date: string) {
+    return this.http.get("/report/other/platform-data", {date: date});
+  }
+
+  //导出报表
+  exportDataReport(city: string, date: string) {
+    location.href = ApiUrl + "/report/other/download-every-day-excel?date=" + date + "&city=" + city;
+  }
 
 }
