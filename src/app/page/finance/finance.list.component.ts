bcb0e37721b84e6684afcdea1d60ba70bbfa2da9
Jarven <769558579@qq.com>
2017-01-13 15:16:19 +0800

    1.财务统计模块

--- src/app/page/finance/finance.list.component.ts
+++ src/app/page/finance/finance.list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FinanceService} from '../../service/finance.service';
 @Component({
   selector: 'finance-list-component',
   templateUrl: './finance.list.component.html'
@@ -8,7 +9,27 @@ export class FinanceListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  constructor() {
+  constructor(private financeService:FinanceService) {
+  }
+
+  //日期对象
+  queryDate: string = '';
+  //Toast提示
+  toastMessage: string = '请选择日期';
+  //类型
+  toastType: string = 'warning';
+  //显示|关闭toast
+  showAlert: boolean = false;
+
+  //触发查询操作
+  receiveDate(event: any) {
+    this.queryDate = event;
+
+  }
+
+  //toast通知
+  notifyParamFunction(event) {
+    this.showAlert = !this.showAlert;
   }
 
 }
