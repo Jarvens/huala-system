819be52034aed77f17a1217191e87d36ba92b05c
Jarven <769558579@qq.com>
2017-01-16 17:27:27 +0800

    1.系统配置模块 模态

--- src/app/page/system/config/config.component.ts
+++ src/app/page/system/config/config.component.ts
@@ -19,6 +19,10 @@ export class ConfigComponent implements OnInit {
   pageOpts: any = {page: 1, total: 0, limit: 3, perPage: 10};
   //搜索关键字
   key: string = '';
+  //搜搜提示
+  placeholder: string = '搜索..';
+  //配置项模态  打开|关闭
+  configOpen: boolean = false;
 
   //分页
   pageChange(event: any) {
@@ -28,7 +32,7 @@ export class ConfigComponent implements OnInit {
 
   //编辑
   edit(data: any) {
-
+    this.configOpen = !this.configOpen;
   }
 
   //查询方法
@@ -42,5 +46,10 @@ export class ConfigComponent implements OnInit {
   searchByCondition(data: any) {
     this.key = data;
     this.queryConfig(data, this.pageOpts);
+  }
+
+  //模态cancel
+  cancel() {
+    this.configOpen = !this.configOpen;
   }
 }
