381d1f94c1f1f3018a779f7f5f418e6e4201a1cd
Jarven <769558579@qq.com>
2017-01-17 10:08:38 +0800

    1.岗位列表

--- src/app/page/hng/job/hng.job.component.ts
+++ src/app/page/hng/job/hng.job.component.ts
@@ -1,11 +1,42 @@
-import {Component,OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HngService} from '../../../service/hng.service';
 @Component({
-  selector:'hng-job-component',
-  templateUrl:'./hng.job.component.html'
+  selector: 'hng-job-component',
+  templateUrl: './hng.job.component.html'
 })
 
-export class HngJobComponent implements OnInit{
-  ngOnInit(): void {
+export class HngJobComponent implements OnInit {
+  constructor(private hngService: HngService) {
   }
 
+  ngOnInit(): void {
+    this.queryJobList(this.key, this.pageOpts);
+  }
+
+  //岗位列表
+  jdbList: any = {};
+  //分页对象
+  pageOpts: any = {page: 1, total: 0, limit: 3, perPage: 10};
+  //搜索条件
+  key: string = '';
+  placeholder: string = '搜索..岗位名称..联系人..电话';
+
+  //条件搜索
+  searchByCondition(data: any) {
+    this.key = date;
+    this.queryJobList(this.key, this.pageOpts);
+  }
+
+  //查询岗位列表
+  queryJobList(key: string, page: any) {
+    this.hngService.getJobList(key, page).subscribe(res=> {
+      this.jdbList = res.json();
+    });
+  }
+
+  //分页
+  pageChange(data: any) {
+    this.pageOpts.page = data;
+    this.queryJobList(this.key, this.pageOpts);
+  }
 }
--- src/app/service/hng.service.ts
+++ src/app/service/hng.service.ts
@@ -2,7 +2,13 @@ import {Injectable} from '@angular/core';
 import {MyHttp} from '../core/http';
 @Injectable()
 export class HngService {
-constructor(private http:MyHttp){
+  constructor(private http: MyHttp) {
+  }
 
-}
+  getJobList(key: string, page: any) {
+    if (!page) {
+      page = {page: 1, perPage: 10};
+    }
+    return this.http.get("/recruit/list-job", {page: page.page, size: page.perPage, searchKey: key});
+  }
 }
