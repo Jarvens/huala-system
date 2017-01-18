ba5739508f9ef1c61039c9ef813cb6c6f90805f7
Jarven <769558579@qq.com>
2017-01-18 14:38:04 +0800

    1.菜单管理

--- src/app/page/system/menu/sys.menu.component.ts
+++ src/app/page/system/menu/sys.menu.component.ts
@@ -1,11 +1,25 @@
-import {Component,OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SysMenuService} from '../../../service/sys.menu.service';
 @Component({
-  selector:'sys-menu-component',
-  templateUrl:'./sys.menu.component.html'
+  selector: 'sys-menu-component',
+  templateUrl: './sys.menu.component.html'
 })
 
-export class  SysMenuComponent implements OnInit{
+export class SysMenuComponent implements OnInit {
   ngOnInit(): void {
+    this.sysMenuService.getMenuList().subscribe(res=> {
+      this.sysMenuList = res.json();
+    });
   }
+
+  constructor(private sysMenuService: SysMenuService) {
+  }
+
+  //菜单列表
+  sysMenuList: Array<any> = [];
+  //显示  *
+  required: boolean = true;
+
+
 
 }
--- src/app/service/sys.menu.service.ts
+++ src/app/service/sys.menu.service.ts
@@ -4,4 +4,8 @@ import {MyHttp} from '../core/http';
 export class SysMenuService {
   constructor(private http:MyHttp){}
 
+  getMenuList(){
+    return this.http.get("/menu/menu-list");
+
+  }
 }
