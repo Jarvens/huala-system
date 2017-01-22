a29056ef70320c2f70bc3f9b2dcb38b3768496bc
Jarven <769558579@qq.com>
2017-01-22 18:08:58 +0800

    1.秒杀活动管理

--- src/app/page/spike/spike.list.component.ts
+++ src/app/page/spike/spike.list.component.ts
@@ -15,12 +15,23 @@ export class SpikeListComponent implements OnInit {
 
   //秒杀商品对象
   spikeArray: Array<any> = [];
+  //秒杀编辑模态 打开|关闭
+  opened: boolean = false;
+  //操作对象
+  operaObj: any = {};
+  //显示 *
+  required:boolean =true;
 
   //获取秒杀商品列表
   getSpikeList() {
     this.spikeService.getSpikeList().subscribe(res=> {
       this.spikeArray = res.json();
     });
+  }
+
+  edit(data: any) {
+    this.operaObj = data;
+    this.opened = !this.opened;
   }
 
 }
