f6127095efaf6e1d336a83b86a2852587acb9aba
Jarven <769558579@qq.com>
2017-01-22 13:46:02 +0800

    1.秒杀活动管理

--- src/app/page/spike/spike.list.component.ts
+++ src/app/page/spike/spike.list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {SpikeService} from '../../service/spike.service';
 @Component({
   selector: 'spike-list-component',
   templateUrl: './spike.list.component.html'
@@ -6,9 +7,20 @@ import {Component, OnInit} from '@angular/core';
 
 export class SpikeListComponent implements OnInit {
   ngOnInit(): void {
+    this.getSpikeList();
+  }
+
+  constructor(private spikeService: SpikeService) {
   }
 
   //秒杀商品对象
   spikeArray: Array<any> = [];
+
+  //获取秒杀商品列表
+  getSpikeList() {
+    this.spikeService.getSpikeList().subscribe(res=> {
+      this.spikeArray = res.json();
+    });
+  }
 
 }
--- src/app/service/spike.service.ts
+++ src/app/service/spike.service.ts
@@ -3,4 +3,8 @@ import {MyHttp} from '../core/http';
 @Injectable()
 export class SpikeService{
   constructor(private http:MyHttp){}
+
+  getSpikeList(){
+    return this.http.get('/spike/activity-list');
+  }
 }
