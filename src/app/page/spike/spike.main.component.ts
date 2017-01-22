f766ab3a42e324ce3463631c96a182c1b9676bdc
Jarven <769558579@qq.com>
2017-01-22 13:08:13 +0800

    1.秒杀活动管理

---
+++ src/app/page/spike/spike.main.component.ts
@@ -0,0 +1,11 @@
+import {Component,OnInit} from '@angular/core';
+@Component({
+  selector:'spike-main-component',
+  templateUrl:'./spike.main.component.html'
+})
+
+export  class SpikeMainComponent implements OnInit{
+  ngOnInit(): void {
+  }
+
+}
