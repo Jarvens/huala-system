8658bae1331a4b783ea10b63be29ce404d872066
Jarven <769558579@qq.com>
2017-01-23 13:07:30 +0800

    1.seller  组件

--- src/app/page/component/seller/seller.list.common.component.ts
+++ src/app/page/component/seller/seller.list.common.component.ts
@@ -6,14 +6,17 @@ import {SellerService} from '../../../service/seller.service';
 })
 
 export class SellerListCommonComponent implements OnInit {
-  //商家列表
-  sellerList: any = {};
-  //分页对象
-  pageOpts: any = {total: 0, limit: 3, perPage: 10};
   //显示|隐藏  复选框
   @Input() openCheckBox: boolean = true;
   //显示|隐藏  搜索框选项图标
   @Input() openConditionIcon: boolean = true;
+  //搜索延迟时间  毫秒
+  @Input() debounce: number = 2000;
+  //商家列表
+  sellerList: any = {};
+  //分页对象
+  pageOpts: any = {page: 1, total: 0, limit: 3, perPage: 10};
+  //搜索关键字
   searchKey: string = '';
   //全部:0   所属人:1
   keyType: string = '0';
@@ -21,6 +24,7 @@ export class SellerListCommonComponent implements OnInit {
   openSelectAll: boolean = true;
   //搜索提示
   placeholder: string = '搜索  ID  名称 手机号';
+
 
   ngOnInit(): void {
     this.querySellerList(this.searchKey, this.pageOpts, this.keyType);
@@ -33,6 +37,18 @@ export class SellerListCommonComponent implements OnInit {
   querySellerList(key: string, page: any, keyType: string) {
     this.sellerService.getSellerList(page, key, keyType).subscribe(res=> {
       this.sellerList = res.json();
+      console.log(res.json());
     });
   }
+
+  //异步查询事件
+  searchByCondition(data: any) {
+
+  }
+
+  //行点击事件
+  onRowClick(event: any) {
+
+  }
+
 }
