d5e9ca85cf26f304afce4a3dbaf773810e9939c1
Jarven <769558579@qq.com>
2017-02-04 17:59:57 +0800

    1.招聘

--- src/app/page/hng/recruit/hng.recruit.choosetime.component.ts
+++ src/app/page/hng/recruit/hng.recruit.choosetime.component.ts
@@ -1,16 +1,18 @@
 import {Component} from '@angular/core';
 @Component({
-  selector:'choose-time',
-  templateUrl:'./hng.recruit.choosetime.component.html'
+  selector: 'choose-time',
+  templateUrl: './hng.recruit.choosetime.component.html'
 })
-export class RecruitChooseTimeComponent{
+export class RecruitChooseTimeComponent {
   wriInfoStatus: string = 'complete';
   relativeSellerInfoStatus: string = 'complete';
   wriPublishTimeStatus: string = 'doing';
   //格式化时间
-  _date_formate:string = 'yyyy-mm-dd';
+  _date_formate: string = 'yyyy-mm-dd';
+  //招聘模板id
+  recruitId: string = '111';
   //开始时间
-  receiveDate(event:any){
+  receiveDate(event: any) {
 
   }
 
