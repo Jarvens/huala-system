d3d579500bb0887ef96554cbb83b8fdf5a29fe47
Jarven <769558579@qq.com>
2017-02-04 09:14:36 +0800

    1.修复  秒杀活动  AOT打包

--- src/app/page/spike/spike.list.component.ts
+++ src/app/page/spike/spike.list.component.ts
@@ -20,7 +20,18 @@ export class SpikeListComponent implements OnInit {
   //操作对象
   operaObj: any = {};
   //显示 *
-  required:boolean =true;
+  required: boolean = true;
+  //prompt提示消息
+  promptMessage: string = '';
+  //打开|关闭 prompt
+  notificationOpen: boolean = false;
+  //toast类型
+  toastType: string = 'success';
+  //toast提示信息
+  toastMessage: string = '';
+  //打开|关闭toast
+  showAlert: boolean = false;
+
 
   //获取秒杀商品列表
   getSpikeList() {
@@ -32,6 +43,21 @@ export class SpikeListComponent implements OnInit {
   edit(data: any) {
     this.operaObj = data;
     this.opened = !this.opened;
+  }
+
+  //prompt取消事件
+  cancelPrompt() {
+
+  }
+
+  //prompt确认事件
+  confirm() {
+
+  }
+
+  //toast传播事件
+  notifyParamFunction(data: any) {
+
   }
 
 }
