503948e538964f32456ce170741c7f823e88399e
Jarven <769558579@qq.com>
2017-02-04 15:45:26 +0800

    1.招聘信息  头部组件提取

--- src/app/page/hng/recruit/hng.recruit.basic.component.ts
+++ src/app/page/hng/recruit/hng.recruit.basic.component.ts
@@ -1,10 +1,14 @@
-import {Component,OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 @Component({
-  selector:'hng-recruit-basic-component',
-  templateUrl:'./hng.recruit.basic.component.html'
+  selector: 'hng-recruit-basic-component',
+  templateUrl: './hng.recruit.basic.component.html'
 })
 
-export class HngRecruitBasicComponent implements OnInit{
+export class HngRecruitBasicComponent implements OnInit {
+  wriInfoStatus: string = 'doing';
+  relativeSellerInfoStatus: string = 'will';
+  wriPublishTimeStatus: string = 'will';
+
   ngOnInit(): void {
   }
 
