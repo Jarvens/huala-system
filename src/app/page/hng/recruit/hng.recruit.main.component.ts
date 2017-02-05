88ab2ccd3c7454cca1c5bb402997c8a5c410dcfb
Jarven <769558579@qq.com>
2017-02-05 09:41:52 +0800

    1.招聘信息

--- src/app/page/hng/recruit/hng.recruit.main.component.ts
+++ src/app/page/hng/recruit/hng.recruit.main.component.ts
@@ -1,36 +1,41 @@
 import {Component, OnInit} from '@angular/core';
 import {HngService} from '../../../service/hng.service';
 @Component({
-    selector: 'hng-recruit-main-component',
-    templateUrl: './hng.recruit.main.component.html'
+  selector: 'hng-recruit-main-component',
+  templateUrl: './hng.recruit.main.component.html'
 })
 
 export class HngRecruitMainComponent implements OnInit {
-    
-    //招聘信息列表对象
-    hngRecruitDataList: any = {};
-    
-    //分页对象
-    pageOpts: any = {page: 1, total: 0, limit: 3, perPage: 10}
-    
-    constructor(private hngService: HngService) {
-    }
-    
-    ngOnInit(): void {
-    }
-    
-    //分页事件
-    pageChange(event) {
-        this.pageOpts.page = event;
-    }
-    
-    querySellerDataByBtn() {
-        
-    }
-    
-    exportExcel() {
-        
-    }
-    
-    
+
+  //招聘信息列表对象
+  hngRecruitDataList: any = {};
+
+  //分页对象
+  pageOpts: any = {page: 1, total: 0, limit: 3, perPage: 10};
+  //查询条件对象
+  conditions: any = {};
+
+  constructor(private hngService: HngService) {
+  }
+
+  ngOnInit(): void {
+    this.querySellerDataByBtn();
+  }
+
+  //分页事件
+  pageChange(event) {
+    this.pageOpts.page = event;
+  }
+
+  querySellerDataByBtn() {
+    this.hngService.getRecruitData(this.pageOpts, this.conditions).subscribe(res=> {
+      this.hngRecruitDataList = res.json();
+    });
+  }
+
+  exportExcel() {
+
+  }
+
+
 }
--- src/app/service/hng.service.ts
+++ src/app/service/hng.service.ts
@@ -3,7 +3,8 @@ import {MyHttp} from '../core/http';
 
 @Injectable()
 export class HngService {
-  constructor(private http: MyHttp){}
+  constructor(private http: MyHttp) {
+  }
 
   getJobList(key: string, page: any) {
     if (!page) {
@@ -13,19 +14,19 @@ export class HngService {
   }
 
 
-  deleteJob(data:any){
-    return this.http.post("/recruit/delete-job",data);
+  deleteJob(data: any) {
+    return this.http.post("/recruit/delete-job", data);
   }
 
-  saveJob(data:any){
-    return this.http.post("/recruit/save-job",data);
+  saveJob(data: any) {
+    return this.http.post("/recruit/save-job", data);
   }
 
   /*
    * @Description: Get companies list;
    * @Date: 2017-01-16;
    */
-  getCompanies(data){
+  getCompanies(data) {
     let gUrl = "/recruit/list-company";
 
     return this.http.get(gUrl, data);
@@ -35,7 +36,7 @@ export class HngService {
    * @Description: Delete the company;
    * @Date: 2017-01-17;
    */
-  deleteCompany(pData){
+  deleteCompany(pData) {
     let pUrl = "/recruit/del-company";
 
     return this.http.post(pUrl, pData);
@@ -45,7 +46,7 @@ export class HngService {
    * @Description: Add a new company;
    * @Date: 2017-01-17;
    */
-  addCompany(pData){
+  addCompany(pData) {
     let pUrl = "/recruit/save-company";
 
     return this.http.post(pUrl, pData);
@@ -55,7 +56,23 @@ export class HngService {
    * @Description: Add a new company;
    * @Date: 2017-01-17;
    */
-  updateCompany(pData){
+  updateCompany(pData) {
     return this.addCompany(pData);
+  }
+
+  //查询招聘信息列表
+  getRecruitData(page: any, conditions: any) {
+    if (!page) {
+      page = {page: 1, perPage: 10};
+    }
+    return this.http.get("/recruit/list-recruit", {
+      page: page.page,
+      size: page.perPage,
+      key: conditions.key,
+      status: conditions.status,
+      companyId: conditions.companyId,
+      jobId: conditions.jobId,
+      sellerScope: conditions.sellerScope
+    });
   }
 }
