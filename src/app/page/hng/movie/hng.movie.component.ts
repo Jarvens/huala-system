8eabc8cbd27f83283c7d510a537567414be2851d
Jarven <769558579@qq.com>
2017-02-07 16:39:14 +0800

    1.电影活动

--- src/app/page/hng/movie/hng.movie.component.ts
+++ src/app/page/hng/movie/hng.movie.component.ts
@@ -1,18 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 @Component({
   selector: 'hng-movie-component',
   templateUrl: './hng.movie.component.html'
 })
 
-export class HngMovieComponent implements OnInit {
+export class HngMovieComponent implements OnInit,OnChanges {
   //当前活动对象
   currentActiveObj: any = {};
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    let _value = changes['currentActiveObj'];
+    if (_value && _value.currentValue != _value.previousValue) {
+      console.log("改变了->");
+    }
+  }
+
   getActive(data: any) {
     this.currentActiveObj = data;
-    console.log("当前活动对象 ->",data);
   }
 }
--- src/app/page/hng/movie/hng.movie.entry.component.ts
+++ src/app/page/hng/movie/hng.movie.entry.component.ts
@@ -1,4 +1,4 @@
-import {Component,Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 @Component({
   selector: 'hng-movie-entry-component',
   templateUrl: './hng.movie.entry.component.html'
@@ -8,8 +8,10 @@ export class HngMovieEntryComponent {
   //当前商家
   public currentSeller: any = {};
   //当前活动对象
-  @Input() currentActiveObj:any={};
-
+  @Input() currentActiveObj: any = {};
+  public activityData: any = {};
+  //影片信息模态  打开|关闭
+  public opened: boolean = false;
 
   //关联商家事件
   targetFunction(data: any) {
