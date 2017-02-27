417e3ecdf7d28d5b61988ac45a47a013669d122a
Jarven <769558579@qq.com>
2017-02-27 14:11:47 +0800

    1.重构代码

--- src/app/page/basicreport/basic.report.component.ts
+++ src/app/page/basicreport/basic.report.component.ts
@@ -25,6 +25,27 @@ export class BasicReportComponent implements OnInit {
    */
   currentSqlObj: any = {};
 
+  placeholder: string = '参数..';
+
+  /**
+   * 表头
+   * @type {Array}
+   */
+  titles: Array<string> = [];
+  /**
+   * 值
+   * @type {Array}
+   */
+  values: Array<any> = [];
+
+  sqlData: any = {};
+
+  /**
+   * 分页对象
+   * @type {{page: number; total: number; limit: number; perPage: number}}
+   */
+  pageOpts: any = {page: 1, total: 0, limit: 3, perPage: 10}
+
   constructor(private basicReportService: BasicReportService) {
   }
 
@@ -45,6 +66,65 @@ export class BasicReportComponent implements OnInit {
   execute(data: any) {
     this.opened = !this.opened;
     this.currentSqlObj = data;
+    this.sqlInfo(this.pageOpts, this.currentSqlObj);
+  }
+
+  /**
+   * 异步查询
+   * @param data
+   */
+  searchByCondition(data: string) {
+
+  }
+
+  /**
+   * 基础统计详情
+   * @param page
+   * @param data
+   */
+  sqlInfo(page: any, data: any) {
+    let condition: any = {};
+    condition.sqlName = data.sql_name;
+    condition.key = data.group_key + '.' + data.sql_key;
+    condition.param = data.param;
+    this.basicReportService.executeSql(page, condition).subscribe(res=> {
+      this.sqlData = res.json();
+      this.convertToRealData(this.sqlData);
+    });
+  }
+
+  /**
+   * 将查询结果集转换为实际格式的数据
+   * @param data
+   */
+  convertToRealData(data: any) {
+    this.titles = [];
+    this.values = [];
+    let that = this;
+    let title: any = data.rows[0];
+    for (let value in title) {
+      this.titles.push(value);
+    }
+    data.rows.forEach(function (obj: any) {
+      let value: Array<any> = [];
+      for (let key in obj) {
+        value.push(obj[key]);
+      }
+      that.values.push(value);
+    });
+  }
+
+  /**
+   * 分页查询
+   * @param data
+   */
+  pageChange(data: number) {
+    this.pageOpts.page = data;
+    this.sqlInfo(this.pageOpts, this.currentSqlObj);
+  }
+
+  cancel() {
+    this.opened = !this.opened;
   }
 
 }
--- src/app/service/basic.report.service.ts
+++ src/app/service/basic.report.service.ts
@@ -13,5 +13,12 @@ export class BasicReportService {
     return this.http.get('/tongji/sql');
   }
 
+  executeSql(page: any, data: any) {
+    if (!page) {
+      page = {page: 1, perPage: 10};
+    }
+    return this.http.get('/tongji/sqlExe?', {key: data.key, param: data.param, page: page.page, size: page.perPage});
+  }
+
 
 }
